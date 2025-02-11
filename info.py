dc144a0a3476b9065d5b7edcc04c3f315c7ae4df
Ak all bots Control <akallbotscontrol@gmail.com>
2025-02-11 10:50:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,10 +7,10 @@
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", ""))
-API_HASH     = environ.get("API_HASH", "")
+API_ID       = int(environ.get("API_ID", "22289037"))
+API_HASH     = environ.get("API_HASH", "ced5a994db436661f9af63c4b5247ac6")
 BOT_TOKEN    = environ.get("BOT_TOKEN", "")
 DATABASE_URI = environ.get("DATABASE_URI", "")
-LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", ""))
-ADMIN        = int(environ.get("ADMIN", ""))
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002273915928"))
+ADMIN        = int(environ.get("ADMIN", "7037505654"))
 CHANNEL      = environ.get("CHANNEL", "@RMCBACKUP")
